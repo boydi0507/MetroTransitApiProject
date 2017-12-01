d88a1335b71868965a8619534d028869a649bf95
boydi0507 <boydi0507@my.uwstout.edu>
2017-11-30 20:28:56 -0600

    Now I can print info about a specific bus at a specific stop. Hardcoded.

--- main.rb
+++ main.rb
@@ -1,6 +1,11 @@
 require 'httparty'
+require 'json'
 
 url = 'http://svc.metrotransit.org/NexTrip/11862?format=json'
 response = HTTParty.get(url)
-response.parsed_response
-puts response
+data = JSON.parse(response.body)
+data.each do |bus|
+	if bus['Route'] == "21"
+		puts "Bus route 21 will be here in " + bus['DepartureText']
+	end
+end
