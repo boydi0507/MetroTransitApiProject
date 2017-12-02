33fad1b1bf6c676b3acdc7af5c2009b7e570b7a6
boydi0507 <boydi0507@my.uwstout.edu>
2017-12-02 14:15:45 -0600

    Final product! It is crude but it works for now.

--- main.rb
+++ main.rb
@@ -3,13 +3,16 @@ require 'json'
 
 file = File.read('buses.json')
 data_hash = JSON.parse(file)
-data_hash.each do |stop|
-	url = 'http://svc.metrotransit.org/NexTrip/' + stop['Stop'] + '?format=json'
-	response = HTTParty.get(url)
-	data = JSON.parse(response.body)
-	data.each do |bus|
-		if (bus['Route'] == stop['Route']) && (bus['RouteDirection'] == stop['RouteDirection'])
-			puts 'Bus route ' + bus['Route'] + ' going ' + bus['RouteDirection'] + ' towards ' + bus['Description']  + ' at stop ' + stop['Stop'] + ': '  + bus['DepartureText']
+while true do
+	data_hash.each do |mystops|
+		url = 'http://svc.metrotransit.org/NexTrip/' + mystops['Stop'] + '?format=json'
+		response = HTTParty.get(url)
+		data = JSON.parse(response.body)
+		data.each do |api|
+			if (api['Route'] == mystops['Route']) && (api['RouteDirection'] == mystops['RouteDirection'])
+				puts 'Bus route ' + api['Route'] + ' going ' + api['RouteDirection'] + ' towards ' + api['Description']  + ' at stop ' + mystops['Stop'] + ': '  + api['DepartureText']
+			end
 		end
 	end
+	sleep(30)
 end
