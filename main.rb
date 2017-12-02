84901915fb89ba65f1a6e4357dd523f4b139912b
boydi0507 <boydi0507@my.uwstout.edu>
2017-12-02 10:26:21 -0600

    More detailed print + logic

--- main.rb
+++ main.rb
@@ -1,11 +1,15 @@
 require 'httparty'
 require 'json'
 
-url = 'http://svc.metrotransit.org/NexTrip/11862?format=json'
-response = HTTParty.get(url)
-data = JSON.parse(response.body)
-data.each do |bus|
-	if bus['Route'] == "21"
-		puts "Bus route 21 will be here in " + bus['DepartureText']
+file = File.read('buses.json')
+data_hash = JSON.parse(file)
+data_hash.each do |stop|
+	url = 'http://svc.metrotransit.org/NexTrip/' + stop['Stop'] + '?format=json'
+	response = HTTParty.get(url)
+	data = JSON.parse(response.body)
+	data.each do |bus|
+		if (bus['Route'] == stop['Route']) && (bus['RouteDirection'] == stop['RouteDirection'])
+			puts 'Bus route ' + bus['Route'] + ' going ' + bus['RouteDirection'] + ' towards ' + bus['Description']  + ' at stop ' + stop['Stop'] + ': '  + bus['DepartureText']
+		end
 	end
 end
